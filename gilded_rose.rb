ffb90eb4efaf2abe096a3d4ff9cd26d15ba83f7c
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2017-11-22 13:54:24 +0100

    Object based refactoring

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,66 +1,71 @@
 class ItemWrapper < SimpleDelegator
-  def restore!(v = 1)
-    return if __getobj__.quality >= 50
-    __getobj__.quality += v
-    yield if block_given?
+  def offset_quality!(v = 1)
+    new_quality = quality + v
+    return unless (0..50).cover?(new_quality)
+    __getobj__.quality = new_quality
   end
-
-  def degrade!(v = 1)
-    return unless quality.positive?
-    return if sulfuras?
-    __getobj__.quality -= v
-  end
-
-  AGED_BRIE = 'Aged Brie'
-  BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert'
-  SULFURAS = 'Sulfuras, Hand of Ragnaros'
 
   def age!
-    return if sulfuras?
     __getobj__.sell_in -= 1
   end
 
-  def sulfuras?
-    name == SULFURAS
+  def update!
+    age!
+    offset_quality!(sell_in.negative? ? -2 : -1)
+  end
+
+  def self.match?(item)
+    item.name == self::NAME
   end
 end
 
-def update_aged_brie(item)
-  item.age!
-  item.restore!(item.sell_in.negative? ? 2 : 1)
+class SulfurasWrapper < ItemWrapper
+  NAME = 'Sulfuras, Hand of Ragnaros'
+  def update!;end
 end
 
-def backstage_restore_amount(item)
-  if item.sell_in <= -1
-    -item.quality
-  elsif (5..9).cover?(item.sell_in)
-    2
-  elsif (0..4).cover?(item.sell_in)
-    3
-  else
-    1
+class AgedBrieWrapper < ItemWrapper
+  NAME = 'Aged Brie'
+  def update!
+    age!
+    offset_quality!(sell_in.negative? ? 2 : 1)
   end
 end
 
-def update_backstage_passes(item)
-  item.age!
-  item.restore!(backstage_restore_amount(item))
+class BackStagePassesWrapper < ItemWrapper
+  NAME = 'Backstage passes to a TAFKAL80ETC concert'
+  def offset_quality_amount
+    if sell_in <= -1
+      -quality
+    elsif (5..9).cover?(sell_in)
+      2
+    elsif (0..4).cover?(sell_in)
+      3
+    else
+      1
+    end
+  end
+
+  def update!
+    age!
+    offset_quality!(offset_quality_amount)
+  end
 end
 
-def update_standard(item)
-  item.age!
-  item.degrade!(item.sell_in.negative? ? 2 : 1)
+module WrapperFactory
+  WRAPPERS = [
+    SulfurasWrapper,
+    AgedBrieWrapper,
+    BackStagePassesWrapper
+  ].freeze
+
+  def self.build(item)
+    wrapper = (WRAPPERS.find { |w| w.match?(item) } || ItemWrapper).new(item)
+  end
 end
 
 def update_item(item)
-  case item.name
-  when ItemWrapper::AGED_BRIE
-    update_aged_brie(item)
-  when ItemWrapper::BACKSTAGE_PASSES
-    update_backstage_passes(item)
-  else
-    update_standard(item)
-  end
+  WrapperFactory.build(item).update!
 end
 
 def update_quality(items)
